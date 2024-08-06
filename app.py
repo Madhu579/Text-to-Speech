dc7e72fa9c4626d843a71bc502ed944863fae06e
MADHU MALATHI B N <119298947+Madhu579@users.noreply.github.com>
2024-08-06 19:36:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, send_file, send_from_directory
+from flask import Flask, render_template, request, send_file
 from gtts import gTTS
 import os
 
@@ -6,21 +6,37 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('tts.html')
 
 @app.route('/generate_audio', methods=['POST'])
 def generate_audio():
-    uploaded_file = request.files['file_path']
-    if uploaded_file:
-        file_content = uploaded_file.read().decode("utf-8")
-        output_audio_path = request.form['output_audio_path']
-        output_audio_path = output_audio_path + ".mp3"
-        language = request.form['language']
-        tts = gTTS(text=file_content, lang=language, slow=False)
-        tts.save(output_audio_path)
-        return send_file(output_audio_path, as_attachment=True)
-    else:
-        return "No file uploaded."
+    output_audio_path = request.form['output_audio_path']
+    output_audio_path = output_audio_path + ".mp3"
+    language = request.form['language']
+
+    # Check the option chosen by the user
+    option = request.form['option']
+
+    # If user chooses to upload a file
+    if option == 'file':
+        uploaded_file = request.files['file_path']
+        if uploaded_file:
+            file_content = uploaded_file.read().decode("utf-8")
+            tts = gTTS(text=file_content, lang=language, slow=False)
+            tts.save(output_audio_path)
+            return send_file(output_audio_path, as_attachment=True)
+        else:
+            return "No file uploaded."
+
+    # If user chooses to enter text directly
+    elif option == 'text':
+        text_content = request.form.get('text_content')
+        if text_content:
+            tts = gTTS(text=text_content, lang=language, slow=False)
+            tts.save(output_audio_path)
+            return send_file(output_audio_path, as_attachment=True)
+        else:
+            return "No text provided."
 
 if __name__ == '__main__':
     app.run(debug=True)
